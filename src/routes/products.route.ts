1aa4bb5f1225361b0e2c7e490d5cb1fef8b7987a
AzhamDev <dev.azham@gmail.com>
2024-12-02 13:35:10 +0700

    cookie, headers, status, query

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -1,11 +1,44 @@
-import { Elysia } from "elysia"
+import { Elysia, t } from "elysia"
 
 export const productsRouter = new Elysia({ prefix: "/products" })
-  .get("/", () => {
+  .get("/", ({ cookie: { sessionId }, query }) => {
+    sessionId.set({
+      value: "mySessionId",
+      httpOnly: true,
+    })
+
+    const { category, minP, maxP } = query
+
+    // logic if category = ... -> get data by category
+    // ...
+
+    // logic if minP || && maxP -> get data by price
+
     return {
       message: "You are getting all products",
     }
   })
+
+  .post(
+    "/",
+    ({ body, headers, set }) => {
+      console.log(headers)
+      set.status = 201;
+      return {
+        message: "You are creating a product",
+        body,
+      }
+    },
+    // validation schema guard
+    {
+      body: t.Object({
+        name: t.String(),
+        price: t.Number(),
+        authorEmail: t.String({ format: "email" }),
+      }),
+    }
+  )
+
   .get("/:productId", ({ params }) => {
     const productId = params.productId
     return {
