50f9dc46fed54d6bac055a39878b2e68798d8b5d
AzhamDev <dev.azham@gmail.com>
2024-12-02 13:57:58 +0700

    global and local hooks elysia doesnt have middleware

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,41 @@ import { ordersRouter } from "./routes/orders.route"
 
 // chaining method
 const app = new Elysia()
-  // routes
-  .use(productsRouter)
-  .use(ordersRouter)
+  // Global Hooks
+  .onBeforeHandle(({ headers, set }) => {
+    // if !headers.authorization -> throw 401
+    const { authorization } = headers
+    if (authorization !== "myapitoken") {
+      set.status = 401
+      return {
+        message: "Not authorized, and stop on this point",
+      }
+    }
+
+    // validation
+    // if return something, and stop on this point
+  })
+
+  .onAfterHandle(() => {
+    console.log("After Handle Hook")
+  })
+
+  .get(
+    "/",
+    () => {
+      console.log("Handler")
+      return { message: "You are accessing root!" }
+    },
+    {
+      beforeHandle() {
+        console.log("Before Handle on Local Hook")
+      },
+    }
+  )
+
+// routes
+// .use(productsRouter)
+// .use(ordersRouter)
 
 // runner
 app.listen(process.env.PORT as string, () =>
